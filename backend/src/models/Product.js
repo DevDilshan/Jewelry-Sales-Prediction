baa58ccab03fdc4a8ab0c30ba9e79b61d5d8f090
Rosheni <IT24102050@my.sliit.lk>
2026-02-23 18:08:42 +0530

    Create Product.js
    add product model

---
+++ backend/src/models/Product.js
@@ -0,0 +1,64 @@
+//Es6 
+import mongoose, { Schema } from 'mongoose';
+
+const productSchema = new Schema({
+
+
+  productName :{
+    type:String,
+    required:true,
+    trim:true
+
+  },
+
+  productDescription :{
+    type:String
+  },
+
+  productCategory:{
+    type:String,
+    required:true
+  },
+
+  productPrice :{
+    type:Number,
+    required:true
+
+  },
+
+  productBrand :{
+    type:String,
+    required:true
+  },
+
+  stockQuantity:{
+    type:Number,
+    required:true,
+    default:0
+  },
+  
+  rating:{
+    type:Number,
+    min:0,
+    max:5,
+    default:0
+  },
+
+  productImage:{
+    type:String
+  },
+
+  reorderLevel :{
+    type:Number,
+    default:5
+  },
+  
+  isActive:{
+    type:Boolean,
+    default:true
+  }
+
+
+}, {timestamps: true});
+
+export default mongoose.model("Product", productSchema);
