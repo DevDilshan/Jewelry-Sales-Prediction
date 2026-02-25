d3d66904ac25a8b0de160cd38dd230395f85a468
Uzman Ahamed <167699602+Usman-Ahamed@users.noreply.github.com>
2026-02-25 16:30:41 +0530

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,14 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-export const connectDB = () =>{
+export const connectDB = () => {
     try {
-        mongoose.connect(process.env.MONGO_URI);
-        console.log("MONGODB CONNECTED SUCCESSFULLY")
+        mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("MONGODB CONNECTED SUCCESSFULLY");
     } catch (error) {
-        console.log("MONGODB CONNECTION FAILED")
+        console.log("MONGODB CONNECTION FAILED");
         process.exit(1);
     }
-}
+};
