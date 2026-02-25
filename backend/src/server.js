df2ac414732d0d36013dccf491ebe1630d4dfd14
Uzman Ahamed <167699602+Usman-Ahamed@users.noreply.github.com>
2026-02-25 16:30:09 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import { connectDB } from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import staffRoutes from './routes/staffRoutes.js';
-import customerRoutes from './routes/customerRoutes.js'; 
+import customerRoutes from './routes/customerRoutes.js';  
 
 dotenv.config();
 
@@ -14,18 +14,22 @@ const PORT = process.env.PORT || 5001;
 
 connectDB();
 
+
 app.use(express.json());
 app.use(cors({
     origin: "http://localhost:5173",  
 }));
 
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+
 app.use('/api/auth', authRoutes);  
-app.use('/api/staff', staffRoutes);  
-app.use('/api/customers', customerRoutes);  
+app.use('/api/staff', staffRoutes); 
+app.use('/api/customers', customerRoutes); 
+
 
 app.listen(PORT, () => {
     console.log(`Server started on PORT: ${PORT}`);
