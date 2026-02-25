5b516b34dde3764110ffa4b52d723165223d4103
Uzman Ahamed <167699602+Usman-Ahamed@users.noreply.github.com>
2026-02-25 14:59:08 +0530

    Update customer.js

--- backend/src/models/customer.js
+++ backend/src/models/customer.js
@@ -1,1 +1,18 @@
+import mongoose, { Schema } from 'mongoose';
 
+const customerSchema = new Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phoneNumber: { type: String, required: true },
+    address: { type: String, required: true },
+    isActive: { type: Boolean, default: true },
+    loyaltyPoints: { type: Number, default: 0 }, 
+    membershipTier: { 
+        type: String, 
+        enum: ['Bronze', 'Silver', 'Gold'], 
+        default: 'Bronze' 
+    },
+}, { timestamps: true });
+
+export default mongoose.model("Customer", customerSchema);
