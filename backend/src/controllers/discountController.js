e4ac92970274636cd3f46af9f60f0f32138ac3b5
Jayodya_GVEB <enurijayodya03@gmail.com>
2026-02-26 22:21:57 +0530

    update controller

--- backend/src/controllers/discountController.js
+++ backend/src/controllers/discountController.js
@@ -1,26 +1,98 @@
-import Discount from "../models/Discount.js";
+import Discount from "../models/Discount";
 
-export async function createDiscount(req,res){
+function generateCouponCode(name){
+    const randomCode = Math.floor(1000+Math.random()*9000);
+    return name.toUpperCase().replace(/\s/g,"")+randomCode;
+}
+export async function createDiscount(req,res) {
+    try{
+        const{
+            discountName,
+            disType,
+            disPercentage,
+            startDate,
+            endDate
+        }= req.body;
+
+         if (!discountName || !disType || !disPercentage || !startDate || !endDate) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+        
+           if (new Date(endDate) <= new Date(startDate)) { //extra
+            return res.status(400).json({message: "End date must be after start date"});
+        }
+        let couponCode =null;
+             if(disType==="coupon"){
+                couponCode = generateCouponCode(discountName);
+             }
+
+        const discount = await Discount.create({
+            discountName,
+            disType,
+            disPercentage,
+            couponCode,
+            startDate,
+            endDate
+        });
+        res.status(201).json(discount);
+    }
+
+    catch(error){
+        res.status(500).json({
+            message:"Internal server error",error:error.message });
+    }
+ 
+}
+export async function getAllDiscounts(req, res) {
     try {
-        const discount = await Discount.create(req.body); // get data from req body
-        res.status(201).json({message: "Discount created successfully."}); // create sus
-    } catch (error) {
-        res.status(500).json(error); //internal sever error
-        console.error(error.message);
+
+        const discounts = await Discount.find();
+        res.status(200).json(discounts);
+
+    } 
+    catch (error) {
+        res.status(500).json({
+             message: "Internal server error",error: error.message});
     }
 }
-export async function viewDiscount(req,res){
+export async function updateDiscount(req, res) {
     try {
-        const discount = await Discount.find({}); //{} to get all
-        res.status(200).json(discount)  //ok
+
+        const updated = await Discount.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+
+        if (!updated) {
+            return res.status(404).json({ message: "Discount not found" });
+        }
+
+        res.status(200).json(updated);
+
     } catch (error) {
-        res.status(500).json(error);
-        console.error(error.message);
+        res.status(500).json({
+            message: "Internal server error",
+            error: error.message
+        });
+    }
+
+}
+export async function deleteDiscount(req, res) {
+    try {
+
+        const deleted = await Discount.findByIdAndDelete(req.params.id);
+
+        if (!deleted) {
+            return res.status(404).json({ message: "Discount not found" });
+        }
+
+        res.status(200).json({ message: "Discount deleted successfully" });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Internal server error",error: error.message
+        });
     }
 }
-export async function updateDiscount(req,res) {
-    
-}
-export async function deleteDiscount(req,res) {
-    
-}
+
