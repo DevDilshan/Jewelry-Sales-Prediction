587be07176eaf25f5cb7ff3069e2479e2d327da3
hirandharmarathna <hiranndharmarathna@gmail.com>
2026-02-27 21:37:25 +0530

    Update Routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,29 @@
-import express from "express"
+import express from "express";
 import dotenv from "dotenv";
 import cors from 'cors';
-import {connectDB} from './config/db.js'
-import authRoutes from './routes/authRoutes.js'
-import feedbackRoutes from './routes/feedbackRoutes.js'
+import path from 'path';
+import { connectDB } from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+import feedbackRoutes from './routes/feedbackRoutes.js';
+
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
 
 connectDB();
+
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
-app.use(cors({
-    origin: "http://localhost:5173",
-}));
+app.use('/uploads', express.static('uploads'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.use('/api/auth', authRoutes)
-app.use('/api/review', feedbackRoutes)
+app.use('/api/auth', authRoutes);
+app.use('/api/feedback', feedbackRoutes);
 
-app.listen(PORT, ()=>{
-    console.log("Server started on PORT:",PORT);
-})
-
+app.listen(PORT, () => {
+  console.log("Server started on PORT:", PORT);
+});
