6ececb9a03b845d7bb724760ae494657a5f3e890
hirandharmarathna <hiranndharmarathna@gmail.com>
2026-02-27 20:44:06 +0530

    Update controller

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,6 +1,6 @@
 
 export async function registerUser(req, res){
-
+    
 }
 
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cors from 'cors';
 import {connectDB} from './config/db.js'
 import authRoutes from './routes/authRoutes.js'
+import feedbackRoutes from './routes/feedbackRoutes.js'
 dotenv.config();
 
 const app = express();
@@ -19,6 +20,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/auth', authRoutes)
+app.use('/api/review', feedbackRoutes)
 
 app.listen(PORT, ()=>{
     console.log("Server started on PORT:",PORT);
